ce061d4f92b237cad9726a96284f4a337c05c42f
kale champagnie <neom.bash@gmailc.om>
2017-08-17 14:27:46 -0700

    'Update;

--- public/scripts/core.js
+++ public/scripts/core.js
@@ -1,27 +1,60 @@
-var messages = angular.module('messages', ['ngRoute', 'ngStorage'])
-var socket = io();
+$(function() {
+	var Chirp = angular.module('Chirp'); //['ngRoute', 'ngStorage'])
+	var socket = io();
 
-function mainController($scope, $http, $localStorage){
+	$('.view').hide();       // \____ jQuery should be working but just isn't.
+	$('#start').fadeIn(600); // /
 
-}
 
-function homeController($scope, $http, $localStorage){
-    var messages = []
-    socket.on('message', function(data){
-        $('#messages').append('<li>'+data+'</li>')
-    })
 
-    $scope.send = function(){
-        socket.emit('message', $scope.message)
-        $scope.message = null
-    }
-}
+	Chirp.controller('start', function($scope) {
 
-messages.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "/templates/home.html",
-        controller : homeController
-    })
+	});
+
+	Chirp.controller('create', function($scope) {
+
+	});
+
+	Chirp.controller('join', function($scope) {
+		$scope.login = function() {
+			socket.emit('join', {user: $scope.user, room: $scope.room});
+		}
+	});
+
+	Chirp.controller('chat', function($scope) {
+		socket.on('message', function(data) {
+			$('#messages').append('<li>'+ data + '</li>');
+		});
+ 
+		$scope.send = function() {
+			socket.emit('message', $scope.message);
+		}
+	});
 });
+
+
+
+
+// function mainController($scope, $http, $localStorage){
+
+// }
+
+// function homeController($scope, $http, $localStorage){
+//     socket.on('message', function(data){
+//         $('#messages').append('<li>'+data+'</li>')
+//     })
+
+//     $scope.send = function(){
+//         socket.emit('message', $scope.message)
+//         $scope.message = null
+//     }
+// }
+
+// messages.config(function($routeProvider) {
+//     $routeProvider
+//     .when("/", {
+//         templateUrl : "/templates/index.html",
+//         controller : homeController
+//     })
+// });
 
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var low = require('lowdb');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
-morgan('tiny')
+morgan('tiny');
+
 app.use(express.static(__dirname+'/public'))
 
 app.get('/', function(req, res){
@@ -15,21 +16,24 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  
   socket.on('disconnect', function(){
       console.log('user disconnected');
   });
+  
   socket.on('join', function(data){
-      console.log(data)
-      socket.username = data.username
-      socket.roomname = data.room
-      socket.join(data.room)
-  })
+      console.log(data);
+      socket.user = data.user;
+      socket.room = data.room;
+      socket.join(data.room);
+  });
+  
   socket.on('message', function(data){
-      io.emit('message', data)
-  })
+      io.emit('message', data);
+  });
 });
 
-http.listen(3000, function(){
+server.listen(3000, function(){
   console.log('listening on *:3000');
 });
    
